ec68c3416383784f1bf1915f2a5facf61e74271a
Lucas <Lucas@DESKTOP-TFABCD9>
2021-06-05 22:07:05 -0300

    Configuring Maven

--- TestProject/src/ActionTest.java
+++ TestProject/src/ActionTest.java
@@ -3,9 +3,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
 public class ActionTest {
-
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
--- TestProject/src/Test2.java
+++ TestProject/src/Test2.java
@@ -2,10 +2,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 //clear(), sendKeys(), getAttribute(), click(), getText(), isDisplayed(), isEnabled(), getCssValue()
 public class Test2 {
-
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
